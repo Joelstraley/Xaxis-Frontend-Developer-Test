3ae1f359d73faef7ba3690a31ac33c75644c9c56
Joel Straley <joelstraley@gmail.com>
2023-09-20 21:10:00 -0400

    fixed text change functionality

--- 300x600/script.js
+++ 300x600/script.js
@@ -1,121 +1,55 @@
 const mainText = document.getElementById('main-text-container')
 const body = document.getElementsByTagName('body')[0]
 
-/* 
-Text array
-for-loop - setTimeout to run through array
-if index is at last, run show product function to update background image and remove header
-*/
-
 let textCollection = [
-    `<h1 class="main-text">
+  `<h1 class="main-text">
         Elevate the look of your eyes with LUMIFY eye drops
-    </h1>`, 
+    </h1>`,
 
-    `<h1 class="main-text">
+  `<h1 class="main-text">
         LUMIFY dramatically reduces redness in    
         <span style="font-weight: bold;">1 minute</span>
     </h1>`,
 
-    `<h1 class="main-text">
+  `<h1 class="main-text">
         And lasts up to 
         <span style="font-weight: bold;"> 8 hours</span>
     </h1>`,
 
-    `<h1 class="main-text">
+  `<h1 class="main-text">
         See for yourself
     </h1>`
 ]
 
-//setInterval(5000, showProductImage)
-
-function showProductImage(counter) {
-    console.log("body", body)
-    body.style.backgroundImage = "url('./assets/background--product.png')";
-        mainText.innerHTML = textCollection[textCollection.length - 1]
-        mainText.classList.remove('main-text--hide')
+function showProductImage(text) {
+  body.style.backgroundImage = "url('background--product.png')";
+  
+  /*   mainText.classList.add('main-text--hide')
+    mainText.innerHTML = text
+    mainText.classList.remove('main-text--hide') */
 }
 
-/* if (counter < textCollection.length - 1) {
-    setInterval(rotateThroughText, 2500)
-} else if (counter === textCollection.length - 1) {
-    showProductImage()
-    //clearInterval(timer)
-} */
 
 let counter = 0
 
-function rotateThroughText() {
-    console.log(counter)
-        //mainText.classList.add('main-text--hide')
-        const timer = setTimeout(function () {
-            mainText.innerHTML = textCollection[counter]
-            mainText.classList.remove('main-text--hide')
-            if (counter < textCollection.length - 1){
-                counter++
-            }
-        }, 500)
-
-        if (counter < textCollection.length - 1) {
-            setInterval(rotateThroughText, 2500)
-        }
-
-        if (counter === textCollection.length - 1) {
-            showProductImage(counter)
-            counter++
-        }
-        
-       if (counter > textCollection.length -1){
-            clearInterval(timer)
-        } 
-        ///clearInterval(timer)
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* function updateText(text) {
-    console.log('update', text)
-    const timer = setInterval(() => {
-        mainText.innerHTML = text
-    }, 1000)
-   return clearInterval(timer)
-}
+mainText.innerHTML = textCollection[0]
 
 function rotateThroughText() {
-   // mainText.innerHTML = textCollection[1]
+  ///let time = 2000
 
-    for (let i = 0; i < textCollection.length; i++) {
-        console.log('HTML', mainText, "text", textCollection[i])
-        if (i === textCollection.length -1){
-            showProduct()
-        } 
-            updateText(textCollection[i])
-    } 
-} */
+  text = textCollection[counter]
+  console.log("out", text)
+  setTimeout(() => {
+    mainText.classList.add('main-text--hide')
+    console.log("in", text)
+    mainText.innerHTML = text
+    mainText.classList.remove('main-text--hide')
+    counter++
+    if (counter === textCollection.length) {
+      return showProductImage(text)
+    }
+    rotateThroughText()
+  }, 2000)
+}
 
-//rotateThroughText()
-window.addEventListener('load', rotateThroughText)
-
-
-/* 
-    const timer = setInterval(() => {
-      setDisplayText((prevText) => prevText + flag[currIndex]);
-      setCurrIndex((prevIndex) => prevIndex + 1);
-    }, 500);
-
-    return () => clearInterval(timer);
-*/
+rotateThroughText()
--- README.md
+++ README.md
@@ -4,4 +4,7 @@
 Had to substitute the font for the google font Mulish
 
 
+--"Use As Directed" does not fade and change colors in 160x600 design. Starts as Purple.
+
+
  -->
